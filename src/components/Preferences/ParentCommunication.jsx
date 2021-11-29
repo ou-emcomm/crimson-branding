af615fbdcf5d2307547d63cb69418778e4973d27
Bryce Kunkel <bryce.kunkel@ou.edu>
2021-11-29 11:44:07 -0600

    delete relationships

--- src/components/Preferences/ParentCommunication.jsx
+++ src/components/Preferences/ParentCommunication.jsx
@@ -5,6 +5,8 @@ import Table from 'react-bootstrap/Table';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
 import { faClock } from '@fortawesome/free-solid-svg-icons/faClock';
+import { faMinusCircle } from '@fortawesome/free-solid-svg-icons/faMinusCircle';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons/faTimesCircle';
 import validator from 'email-validator';
 import qs from 'query-string';
 import Toast from './Toast';
@@ -20,39 +22,7 @@ const Parents = function () {
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState('');
 
-  const saveChanges = () => {
-    const data = relationships.filter((el) => el.guid === 'new')[0];
-    const url = process.env.NODE_ENV === 'production'
-      ? '?cmd=addRelation'
-      : 'https://0a4000bc-980e-4144-9a64-c8acf69f392a.mock.pstmn.io/addRelation';
-    fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: qs.stringify({ cmd: 'addRelation', ...data, type: data.typeValue }),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          setShowToast(true);
-          setToastMessage('There was an error saving your changes. Please try again later');
-        }
-      })
-      .then((data) => {
-        const getRelationshipsUrl = process.env.NODE_ENV === 'production'
-          ? '?cmd=getRelationships'
-          : 'https://0a4000bc-980e-4144-9a64-c8acf69f392a.mock.pstmn.io/getRelationships';
-        fetch(getRelationshipsUrl)
-          .then((res) => res.json())
-          .then((result) => {
-            setInitRelationships(result.row);
-            setRelationships(result.row);
-          });
-      })
-      .catch((error) => {
-        setShowToast(true);
-        setToastMessage(`There was an error... ${error}`);
-      });
-  };
-  useEffect(() => {
+  const loadRelationships = () => {
     const getRelationshipsUrl = process.env.NODE_ENV === 'production'
       ? '?cmd=getRelationships'
       : 'https://0a4000bc-980e-4144-9a64-c8acf69f392a.mock.pstmn.io/getRelationships';
@@ -62,6 +32,39 @@ const Parents = function () {
         setInitRelationships(result.row);
         setRelationships(result.row);
       });
+  };
+  const saveChanges = () => {
+    const data = relationships.filter((el) => el.guid === 'new')[0];
+    const url = process.env.NODE_ENV === 'production'
+      ? '?cmd=addRelation'
+      : 'https://0a4000bc-980e-4144-9a64-c8acf69f392a.mock.pstmn.io/addRelation';
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: qs.stringify({
+        cmd: 'addRelation',
+        ...data,
+        type: data.typeValue,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          setShowToast(true);
+          setToastMessage('There was an error saving your changes. Please try again later');
+        }
+      })
+      .then((data) => {
+        setShowToast(true);
+        setToastMessage('Your preferences have been saved!');
+        loadRelationships();
+      })
+      .catch((error) => {
+        setShowToast(true);
+        setToastMessage(`There was an error... ${error}`);
+      });
+  };
+  useEffect(() => {
+    loadRelationships();
     const getRelationshipTypesUrl = process.env.NODE_ENV === 'production'
       ? '?cmd=getRelationshipTypes'
       : 'https://0a4000bc-980e-4144-9a64-c8acf69f392a.mock.pstmn.io/getRelationshipTypes';
@@ -92,6 +95,7 @@ const Parents = function () {
             <th>Relation</th>
             <th>Email</th>
             <th>Status</th>
+            <th />
           </tr>
         </thead>
         <tbody>
@@ -100,6 +104,7 @@ const Parents = function () {
               <td>
                 {obj.guid === 'new' ? (
                   <input
+                    style={{ minWidth: '100px' }}
                     className={`form-control form-control-sm${
                       relationships.filter((el) => el.guid === 'new')[0].first.length > 0
                         ? validFirst
@@ -123,6 +128,7 @@ const Parents = function () {
               <td>
                 {obj.guid === 'new' ? (
                   <input
+                    style={{ minWidth: '100px' }}
                     className={`form-control form-control-sm${
                       relationships.filter((el) => el.guid === 'new')[0].last.length > 0
                         ? validLast
@@ -146,6 +152,7 @@ const Parents = function () {
               <td>
                 {obj.guid === 'new' ? (
                   <select
+                    style={{ minWidth: '100px' }}
                     className={`form-control form-select-sm form-select${
                       relationships.filter((el) => el.guid === 'new')[0].typeValue
                         ? validType
@@ -181,6 +188,7 @@ const Parents = function () {
               <td>
                 {obj.guid === 'new' ? (
                   <input
+                    style={{ minWidth: '150px' }}
                     className={`form-control form-control-sm${
                       relationships.filter((el) => el.guid === 'new')[0].email.length > 3
                         ? validEmail
@@ -202,11 +210,17 @@ const Parents = function () {
                 )}
               </td>
               <td>
-                {obj.confirmed ? (
+                {obj.confirmed === '1' ? (
                   <>
                     <FontAwesomeIcon icon={faCheckCircle} />
                     {' '}
                     Confirmed
+                  </>
+                ) : obj.confirmed === '0' ? (
+                  <>
+                    <FontAwesomeIcon icon={faTimesCircle} />
+                    {' '}
+                    Declined
                   </>
                 ) : obj.guid !== 'new' ? (
                   <>
@@ -216,6 +230,7 @@ const Parents = function () {
                   </>
                 ) : (
                   <button
+                    style={{ minWidth: '150px' }}
                     className="btn btn-secondary rounded-pill"
                     disabled={
                       !validFirst || !validLast || !validType || (!validEmail && 'disabled')
@@ -226,11 +241,49 @@ const Parents = function () {
                   </button>
                 )}
               </td>
+              <td>
+                <button
+                  style={{ minWidth: '100px' }}
+                  className="btn btn-sm btn-outline-secondary rounded-pill"
+                  onClick={() => {
+                    const url = process.env.NODE_ENV === 'production'
+                      ? '?cmd=deleteRelation'
+                      : 'https://0a4000bc-980e-4144-9a64-c8acf69f392a.mock.pstmn.io/deleteRelation';
+                    fetch(url, {
+                      method: 'POST',
+                      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                      body: qs.stringify({
+                        cmd: 'deleteRelation',
+                        guid: obj.guid,
+                      }),
+                    })
+                      .then((res) => {
+                        if (!res.ok) {
+                          setShowToast(true);
+                          setToastMessage(
+                            'There was an error saving your changes. Please try again later',
+                          );
+                        }
+                      })
+                      .then((data) => {
+                        setShowToast(true);
+                        setToastMessage('Your preferences have been saved!');
+                        loadRelationships();
+                      })
+                      .catch((error) => {
+                        setShowToast(true);
+                        setToastMessage(`There was an error... ${error}`);
+                      });
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
           {!relationships.some((el) => el.guid === 'new') && (
             <tr>
-              <td colSpan="5">
+              <td colSpan="6">
                 <span
                   onClick={() => setRelationships([
                     ...relationships,
