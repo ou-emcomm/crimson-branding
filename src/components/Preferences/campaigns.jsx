f61cb8631cd9b02d70cd303ffa9528d18b91f4c5
Bryce Kunkel <bryce.kunkel@ou.edu>
2021-11-30 09:33:20 -0600

    text updates

--- src/components/Preferences/campaigns.jsx
+++ src/components/Preferences/campaigns.jsx
@@ -10,13 +10,7 @@ const campaigns = [
         <p>
           The
           {' '}
-          <a
-            style={{ textDecoration: 'none' }}
-            className="text-secondary"
-            href="https://ou.edu/sfc"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://ou.edu/sfc" target="_blank" rel="noreferrer">
             Student Financial Center
           </a>
           {' '}
@@ -25,10 +19,15 @@ const campaigns = [
           details. Standard text messaging and data rates may apply.
         </p>
         <p>
-          Students may opt-out at any time by responding to the text message. Please note that
-          opting out will remove you from all future communication from the Student Financial Center
-          only. You will continue to receive important emails from the Student Financial Center and
-          other offices, as email remains the official form of communication from the university.
+          Students may opt-out of text messages at any time by navigating to
+          {' '}
+          <a href="">crimson.ou.edu/portal/preferences</a>
+          {' '}
+          and unchecking the SMS box associated
+          with the Student Financial Center. Please note that opting out will remove you from future
+          SMS communication from the Student Financial Center only. You will continue to receive
+          important emails from the Student Financial Center and other offices, as email remains the
+          official form of communication for the university.
         </p>
       </>
     ),
@@ -46,13 +45,7 @@ const campaigns = [
         <p>
           The
           {' '}
-          <a
-            style={{ textDecoration: 'none' }}
-            className="text-secondary"
-            href="https://ou.edu/moneycoach"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://ou.edu/moneycoach" target="_blank" rel="noreferrer">
             OU MidFirst Bank MoneyCoach Program
           </a>
           {' '}
@@ -62,11 +55,15 @@ const campaigns = [
           apply.
         </p>
         <p>
-          Students may opt-out at any time by responding to the text message. Please note that
-          opting out will remove you from all future communication from the OU MidFirst Bank
-          MoneyCoach Program only. You will continue to receive important emails from the MoneyCoach
-          Program and other offices, as email remains the official form of communication from the
-          university.
+          Students may opt-out of text messages at any time by navigating to
+          {' '}
+          <a href="">crimson.ou.edu/portal/preferences</a>
+          {' '}
+          and unchecking the SMS box associated
+          with the OU MidFirst Bank MoneyCoach Program. Please note that opting out will remove you
+          from future SMS communication from the OU MidFirst Bank MoneyCoach Program only. You will
+          continue to receive important emails from the OU MidFirst Bank MoneyCoach Program and
+          other offices, as email remains the official form of communication for the university.
         </p>
       </>
     ),
@@ -84,13 +81,7 @@ const campaigns = [
         <p>
           The
           {' '}
-          <a
-            href="https://ou.edu/registrar"
-            style={{ textDecoration: 'none' }}
-            className="text-secondary"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://ou.edu/registrar" target="_blank" rel="noreferrer">
             Office of the Registrar
           </a>
           {' '}
@@ -99,10 +90,15 @@ const campaigns = [
           important details. Standard text messaging and data rates may apply.
         </p>
         <p>
-          Students may opt-out at any time by responding to the text message. Please note that
-          opting out will remove you from all future communication from the Office of the Registrar
-          only. You will continue to receive important emails from the Office of the Registrar and
-          other offices, as email remains the official form of communication from the university.
+          Students may opt-out of text messages at any time by navigating to
+          {' '}
+          <a href="">crimson.ou.edu/portal/preferences</a>
+          {' '}
+          and unchecking the SMS box associated
+          with the Office of the Registrar. Please note that opting out will remove you from future
+          SMS communication from the Office of the Registrar only. You will continue to receive
+          important emails from the Office of the Registrar and other offices, as email remains the
+          official form of communication for the university.
         </p>
       </>
     ),
